cfbeff554d5d4c56f1c33bfb8904017392b12578
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-14 09:54:58 -0300

    PYTHON

---
+++ exemplo.py
@@ -0,0 +1,22 @@
+nome=input('digite seu nome:')
+print(f'Olá {nome}')
+
+a=5+5
+b=a*5
+print(b)
+
+a=int(input('informe sua nota:'))
+b=int(input('informe sua nota:'))
+c=int(input('informe sua nota:'))
+media = (a+b+c)/3
+print(media)
+
+
+
+peso=float(input('digite seu peso'))
+altura=float(input('digite seu altura'))
+
+
+
+print(1800+(1800*20/20))
+
