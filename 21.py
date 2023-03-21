077dec893dac3a46669c29668097d42a1e378faa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-21 09:41:15 -0300

    aula21/03

---
+++ 21.py
@@ -0,0 +1,40 @@
+x=10
+y=20
+#igual a
+print(x==y) #false
+
+#diferente de
+print(x !=y) #true
+
+x=10
+y=5
+print(x>y)
+
+print(x>=y) #true
+print(x<=y) #false
+
+x=5
+y=10
+print(x<10 and y) #true
+print(x>10 and y<5) #false
+print(x<10 or y <5) #true 
+print(x>10 or y >5) #false
+
+x=5
+y=10
+#not
+print(not(x<10)) #false
+print(not(x>10)) #true
+
+idade=20
+#maior de idade
+idade >=18
+#menor de idade
+idade < 18
+not idade >=18
+
+ligado = true
+ligado != true
+
+idade>=16 and idade <=69
+
---
+++ ex1.0.py
@@ -0,0 +1,15 @@
+#Resolva as Expressões a seguir
+(3>=2) or (1==3) #true 
+(1+1) ==(3*1) #false
+7>8-1 #true
+5+3 ==8 #true
+7%4>3 #false
+5*2 == 20//2 #true
+
+#resolva as expressões a seguir
+false and false #false
+true and false #true
+note note true #true 
+true or false and true #true
+false and (false or true) #false
+
